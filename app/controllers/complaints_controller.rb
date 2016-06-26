90d2ddd7d28f2a2dcbbd223340f1fd6a374812f2
Guilherme Labrego <glabrego@gmail.com>
2016-06-26 15:08:43 -0300

    Users now can register a complaint

---
+++ app/controllers/complaints_controller.rb
@@ -0,0 +1,39 @@
+class ComplaintsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_complaint, only: [:show, :edit, :update]
+
+  def index
+    @complaints = Complaint.all
+  end
+
+  def new
+    @complaint = Complaint.new
+  end
+
+  def create
+    @complaint = Complaint.create(complaint_params)
+    render @complaint
+  end
+
+  def update
+    @complaint.update(complaint_params)
+    render @complaint
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  private
+
+  def set_complaint
+    @complaint = Complaint.find(params[:id])
+  end
+
+  def complaint_params
+    params.require(:complaint)
+          .permit(:subject, :complaint_type, :content)
+  end
+end
---
+++ spec/factories/complaints.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :complaint do
+    subject "MyString"
+    type ""
+    content "MyString"
+  end
+end

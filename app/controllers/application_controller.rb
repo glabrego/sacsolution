033728f41c00811f8d16d8bb2d7daf4be71f1740
Guilherme Labrego <glabrego@gmail.com>
2016-06-27 17:34:13 -0300

    Small tweaks on login page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  layout :login_layout
+
+  protected
+
+  def login_layout
+    if current_user
+      'application'
+    else
+      'login'
+    end
+  end
 end

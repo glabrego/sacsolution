c26d6e2b718889c0530bc2df2876ea0b00bf71b8
Guilherme Labrego <glabrego@gmail.com>
2016-06-29 09:02:25 -0300

    Added complaint types

--- app/models/complaint.rb
+++ app/models/complaint.rb
@@ -1,3 +1,5 @@
 class Complaint < ActiveRecord::Base
   validates :subject, :complaint_type, :content, presence: true
+
+  TYPES = ['Indisponibilidade de serviço', 'Produtos contratados', 'Qualidade do produto', 'Qualidade do serviço', 'Erro de cobrança', 'Atendimento'].freeze
 end
